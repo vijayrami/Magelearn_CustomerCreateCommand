81e0390e3aff400bd3faa892ee0e550468b85279
vijayrami <vijaymrami@gmail.com>
2022-05-17 10:11:25 +0530

    modify some code

--- Console/Command/Create.php
+++ Console/Command/Create.php
@@ -40,13 +40,13 @@ class Create extends Command
 
     /**
      * magelearn:customer:create
-     *   [-f|--customer-firstname CUSTOMER-FIRSTNAME]
-     *   [-l|--customer-lastname CUSTOMER-LASTNAME]
-     *   [-e|--customer-email CUSTOMER-EMAIL]
-     *   [-p|--customer-password CUSTOMER-PASSWORD]
-     *   [-w|--website WEBSITE]
-     *   [-s|--send-email [SEND-EMAIL]]
-     *   [-ns|--newsletter-subscribe [NEWSLETTER-SUBSCRIBE]]
+     *   [-f|--customer-firstname CUSTOMER-FIRSTNAME][REQUIRED]
+     *   [-l|--customer-lastname CUSTOMER-LASTNAME][REQUIRED]
+     *   [-e|--customer-email CUSTOMER-EMAIL][REQUIRED]
+     *   [-p|--customer-password CUSTOMER-PASSWORD][REQUIRED]
+     *   [-w|--website WEBSITE][REQUIRED]
+     *   [-s|--send-email [SEND-EMAIL]][OPTIONAL]
+     *   [-ns|--newsletter-subscribe [NEWSLETTER-SUBSCRIBE]][OPTIONAL]
      *
      * php bin/magento magelearn:customer:create -f "Vijay" -l "Rami" -e "vijay.rami@gmail.com" -p "test123" -w 1
      * php bin/magento magelearn:customer:create -f "Vijay" -l "Rami" -e "vijay.rami@gmail.com" -p "test123" -w 1 -s 1 --newsletter-subscribe 1
@@ -70,13 +70,16 @@ class Create extends Command
         
         if ($customer && $customer->getId()) {
             $output->writeln("<info>Created new user</info>");
-            $output->writeln((string) __("User ID: %1", $customer->getId()));
-            $output->writeln((string) __("First name: %1", $customer->getFirstname()));
-            $output->writeln((string) __("Last name: %1", $customer->getLastname()));
-            $output->writeln((string) __("Email: %1", $customer->getEmail()));
-            $output->writeln((string) __("Website Id: %1", $customer->getWebsiteId()));
+            $output->writeln((string) __("<comment>User ID: %1</comment>", $customer->getId()));
+            $output->writeln((string) __("<comment>First name: %1</comment>", $customer->getFirstname()));
+            $output->writeln((string) __("<comment>Last name: %1</comment>", $customer->getLastname()));
+            $output->writeln((string) __("<comment>Email: %1</comment>", $customer->getEmail()));
+            $output->writeln((string) __("<comment>Website Id: %1</comment>", $customer->getWebsiteId()));
+            if ($input->getOption(Customer::KEY_NEWSLETTER_SUBSCRIBE)) {
+                $output->writeln("<comment>Subscribed to Newsletter</comment>");
+            }
             if ($input->getOption(Customer::KEY_SENDEMAIL)) {
-                $output->writeln("Sending Email");
+                $output->writeln("<comment>Sending Email</comment>");
             }
         } else {
             $output->writeln("<error>Problem creating new user</error>");
@@ -99,8 +102,8 @@ class Create extends Command
             new InputOption(Customer::KEY_EMAIL, '-e', InputOption::VALUE_REQUIRED, '(Required) Email'),
             new InputOption(Customer::KEY_PASSWORD, '-p', InputOption::VALUE_REQUIRED, '(Required) Password'),
             new InputOption(Customer::KEY_WEBSITE, '-w', InputOption::VALUE_REQUIRED, '(Required) Website ID'),
-            new InputOption(Customer::KEY_SENDEMAIL, '-s', InputOption::VALUE_OPTIONAL, '(1/0) Send email (default 0)'),
-            new InputOption(Customer::KEY_NEWSLETTER_SUBSCRIBE, '-ns', InputOption::VALUE_OPTIONAL, '(1/0) Subscribe to Newsletter (default 0)')
+            new InputOption(Customer::KEY_SENDEMAIL, '-s', InputOption::VALUE_OPTIONAL, '(Optional)(1/0) Send email (default 0)'),
+            new InputOption(Customer::KEY_NEWSLETTER_SUBSCRIBE, '-ns', InputOption::VALUE_OPTIONAL, '(Optional)(1/0) Subscribe to Newsletter (default 0)')
         ]);
         parent::configure();
     }
